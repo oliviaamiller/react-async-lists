36b5f0bc859677571289d3da0c4d1fb7d2fff9ff
oliviaamiller <oliviaoliviaolivia@gmail.com>
2022-02-03 12:55:50 -0800

    added loading img

--- src/App.js
+++ src/App.js
@@ -8,36 +8,49 @@ import BooksList from './Books/BooksList';
 import CakesList from './Cakes/CakesList';
 import CandiesList from './Candies/CandiesList';
 import FlowersList from './Flowers/FlowersList';
+import loadImg from './load.gif';
 // import your arrays here
 
 function App() {
 
   const [books, setBooks] = useState([]);
+  const [booksLoading, setBooksLoading] = useState(false);
   const [cakes, setCakes] = useState([]);
+  const [cakesLoading, setCakesLoading] = useState(false);
   const [candies, setCandies] = useState([]);
+  const [candiesLoading, setCandiesLoading] = useState(false);
   const [flowers, setFlowers] = useState([]);
+  const [flowersLoading, setFlowersLoading] = useState(false);
 
   async function fetchBooksData() {
+    setBooksLoading(true);
     const data = await fetchBooks();
 
+    setBooksLoading(false);
     setBooks(data);
   }
 
   async function fetchCakesData() {
+    setCakesLoading(true);
     const data = await fetchCakes();
 
+    setCakesLoading(false);
     setCakes(data);
   }
 
   async function fetchCandiesData() {
+    setCandiesLoading(true);
     const data = await fetchCandies();
 
+    setCandiesLoading(false);
     setCandies(data);
   }
 
   async function fetchFlowersData() {
+    setFlowersLoading(true);
     const data = await fetchFlowers();
 
+    setFlowersLoading(false);
     setFlowers(data);
   }
 
@@ -53,13 +66,15 @@ function App() {
 
   return (
     <div className="App">
-      < BooksList books={books} />
       
-      <CakesList cakes={cakes} />
+      { booksLoading ? <img src={loadImg} /> : < BooksList books={books} /> }
+      
+      { cakesLoading ? <img src={loadImg} /> : <CakesList cakes={cakes} /> }
 
-      <CandiesList candies={candies} />
+      { candiesLoading ? <img src={loadImg} /> : <CandiesList candies={candies} /> }
 
-      <FlowersList flowers={flowers} />
+      { flowersLoading ? <img src={loadImg} /> : <FlowersList flowers={flowers} /> }
+      
     </div>
   );
 }
