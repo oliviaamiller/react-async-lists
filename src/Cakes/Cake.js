3af5f6cb8c598f658b48b2f737e1dc05ddbb2671
oliviaamiller <oliviaoliviaolivia@gmail.com>
2022-02-03 12:20:33 -0800

    mapped through cakes array

--- src/Cakes/Cake.js
+++ src/Cakes/Cake.js
@@ -1,6 +1,12 @@
 import './Cakes.css';
 
-export default function Cake() {
-  return <div></div>;
+export default function Cake({ cake }) {
+  return <div className='cake'>
+    <p>{cake.name}</p>
+    <p>{cake.flavor}</p>
+    <p>{cake.icing}</p>
+    <p>{cake.decoration}</p>
+
+  </div>;
 }
 
--- src/Cakes/CakesList.js
+++ src/Cakes/CakesList.js
@@ -1,6 +1,10 @@
 import Cake from './Cake';
 import './Cakes.css';
 
-export default function CakesList() {
-  return <div></div>;
+export default function CakesList({ cakes }) {
+  return <div className='cakes-list'>
+    {cakes.map((cake, i) =>
+      <Cake key={`${cake}-${i}`} cake={cake} />)}
+
+  </div>;
 }
