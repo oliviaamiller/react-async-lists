f6b68bf43de53e865cad5aa4717729d70c56fe98
oliviaamiller <oliviaoliviaolivia@gmail.com>
2022-02-03 15:21:53 -0800

    added async and await to test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders coffee link', () => {
+test('renders coffee link', async () => {
   render(<App />);
-  const linkElement = screen.getByText(/coffee/i);
+  const linkElement = await screen.getByText(/coffee/i);
   expect(linkElement).toBeInTheDocument();
 });
