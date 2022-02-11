1bb7bae2c4cab5932c47d2b7464a3244f8394552
oliviaamiller <oliviaoliviaolivia@gmail.com>
2022-02-10 17:41:48 -0800

    refactored cbc

--- src/Books/Book.js
+++ src/Books/Book.js
@@ -1,11 +1,34 @@
 import './Books.css';
+import { Component } from 'react';
 
-export default function Book({ book }) {
-  return <div className='book'>
-    <p className='book-title'>{book.title}</p>
-    <p className='book-author'>{book.author}</p>
-    <p>genre: {book.genre}</p>
-    <p>published: {book.year}</p>
-  </div>;
+export default class Book extends Component {
+  // constructor() {
+  //   super();
+  // }
+
+  render() {
+    // const { book } = this.props;
+    
+    return (
+      <div className='book'>
+        <p className='book-title'>{this.props.book.title}</p>
+        <p className='book-author'>{this.props.book.author}</p>
+        <p>genre: {this.props.book.genre}</p>
+        <p>published: {this.props.book.year}</p>
+      </div>
+    ); 
+     
+    
+  }
 }
+
+
+// export default function Book({ book }) {
+//   return <div className='book'>
+//     <p className='book-title'>{book.title}</p>
+//     <p className='book-author'>{book.author}</p>
+//     <p>genre: {book.genre}</p>
+//     <p>published: {book.year}</p>
+//   </div>;
+// }
 
